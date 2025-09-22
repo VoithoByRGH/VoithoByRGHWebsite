d7f61dd18b851098559fa1c9763edfcf99d2811a
robert horne <info@voithobyrgh.com>
2025-09-22 19:09:18 +0000

    Fix Vimeo background: use embed autoplay muted loop

--- src/App.js
+++ src/App.js
@@ -161,13 +161,20 @@ export default function App() {
       >
         {/* Video (bottom layer) */}
         <iframe
-  title="vimeo-background"
-  src="https://player.vimeo.com/video/1120673066?background=1&autoplay=1&muted=1&loop=1"
-  frameborder="0"
-  allow="autoplay; fullscreen"
-  allowfullscreen
-  style="position:absolute;top:0;left:0;width:100%;height:100%;pointer-events:none;"
-></iframe>
+          title="vimeo-background"
+          src="https://player.vimeo.com/video/1120673066?background=1&autoplay=1&muted=1&loop=1"
+          frameBorder="0"
+          allow="autoplay; fullscreen"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            pointerEvents: "none",
+          }}
+        />
 
         {/* One LIGHT overlay for readability (middle layer) */}
         <div className="absolute inset-0 z-10 bg-black/30"></div>
